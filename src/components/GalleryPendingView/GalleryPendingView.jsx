d1819a01a45b26ef2794133c14c81f06a191049c
Yevheniia Malakhova <jen.mala.kh@gmail.com>
2022-04-19 23:18:38 +0300

    task 6

--- src/components/GalleryPendingView/GalleryPendingView.jsx
+++ src/components/GalleryPendingView/GalleryPendingView.jsx
@@ -1,9 +1,9 @@
-import { TailSpin } from 'react-loader-spinner';
+// import { TailSpin } from 'react-loader-spinner';
 
 export const GalleryPendingView = () => {
   return (
     <div>
-      <TailSpin height="100" width="100" color="grey" ariaLabel="loading" />
+      {/* <TailSpin height="100" width="100" color="grey" ariaLabel="loading" /> */}
       Loading ...
     </div>
   );
